b89c982938ab70998fd3d580bf6d50e1ec3fca2f
faishalstwn <169064956+faishalstwn@users.noreply.github.com>
2025-03-22 17:35:40 +0700

    Add JURNAL-Modul6_2311104066 ke folder 06_Grammar_Based_Input_Processing

---
+++ 06_Grammar_Based_Input_Processing/JURNAL-Modul6_2311104066/JURNAL-Modul6_2311104066/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main()
+    {
+        SayaTubeUser user = new SayaTubeUser("Faishal");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            SayaTubeVideo video = new SayaTubeVideo($"Review Film {i} oleh Faishal");
+            user.AddVideo(video);
+        }
+
+        user.PrintAllVideoPlaycount();
+    }
+}
---
+++ 06_Grammar_Based_Input_Processing/JURNAL-Modul6_2311104066/JURNAL-Modul6_2311104066/SayaTubeVideo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        if (string.IsNullOrEmpty(title) || title.Length > 100)
+            throw new ArgumentException("Judul harus antara 1 hingga 100 karakter.");
+
+        Random rand = new Random();
+        this.id = rand.Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public string GetTitle()
+    {
+        return this.title;
+    }
+
+    public int GetPlayCount()
+    {
+        return this.playCount;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        if (count < 0 || count > 10000000)
+            throw new ArgumentOutOfRangeException("Count harus antara 0 dan 10 juta.");
+
+        this.playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"ID: {id}");
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Play Count: {playCount}");
+    }
+}
