7bed3b07f03aecd8bebb9c932b35aa4e022fd6a1
faishalstwn <169064956+faishalstwn@users.noreply.github.com>
2025-03-28 10:46:55 +0700

    Menambahkan Tp7,Tp7(2) dan Tp-Modul7-2311104066 ke folder 07_Runtime_Configuration_Internationalization

---
+++ 07_Runtime_Configuration_Internationalization/Tp7/Tp7/DataMahasiswa2311104066.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Tp7;
+
+public class DataMahasiswa2311104066
+{
+    public Nama Nama { get; set; }  
+    public string Nim { get; set; }
+    public string Fakultas { get; set; }
+    public static void ReadJSON()
+    {
+        string filePath = "tp7_1_2311104066.json"; 
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("File JSON tidak ditemukan!");
+            return;
+        }
+
+        string jsonData = File.ReadAllText(filePath);
+
+        Console.WriteLine("Isi JSON yang dibaca:");
+        Console.WriteLine(jsonData);
+
+        DataMahasiswa2311104066? mahasiswa = JsonConvert.DeserializeObject<DataMahasiswa2311104066>(jsonData);
+
+
+        Console.WriteLine($"Nama {mahasiswa?.Nama?.depan} {mahasiswa?.Nama?.belakang} dengan nim {mahasiswa?.Nim} dari fakultas {mahasiswa?.Fakultas}");
+    }
+}
---
+++ 07_Runtime_Configuration_Internationalization/Tp7/Tp7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Tp7;  // Tambahkan namespace sesuai dengan lokasi class DataMahasiswa2311104066
+
+class Program
+{
+    static void Main()
+    {
+        DataMahasiswa2311104066.ReadJSON();
+
+        Console.WriteLine("\nTekan ENTER untuk keluar...");
+        Console.ReadLine(); // Agar konsol tidak langsung tertutup
+    }
+}
