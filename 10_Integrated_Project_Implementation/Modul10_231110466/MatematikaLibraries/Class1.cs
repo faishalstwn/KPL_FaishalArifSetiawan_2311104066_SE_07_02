9d37091d435fec53f08fae5ffed45357c483690f
faishalstwn <169064956+faishalstwn@users.noreply.github.com>
2025-05-18 00:49:39 +0700

    Menambahkan Jurnal Modul10_231110466

---
+++ 10_Integrated_Project_Implementation/Modul10_231110466/MatematikaLibraries/Class1.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+using System.Text;
+
+namespace MatematikaLibraries
+{
+    public class Matematika
+    {
+        public static int FPB(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public static int KPK(int a, int b)
+        {
+            return Math.Abs(a * b) / FPB(a, b);
+        }
+
+        public static string Turunan(int[] koefisien)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < koefisien.Length - 1; i++)
+            {
+                int pangkat = koefisien.Length - 1 - i;
+                int koefTurunan = koefisien[i] * pangkat;
+
+                if (koefTurunan == 0) continue;
+
+                if (sb.Length > 0)
+                    sb.Append(koefTurunan > 0 ? " + " : " - ");
+                else if (koefTurunan < 0)
+                    sb.Append("-");
+
+                sb.Append(Math.Abs(koefTurunan));
+                if (pangkat - 1 > 0)
+                    sb.Append("x" + (pangkat - 1));
+            }
+            return sb.ToString();
+        }
+
+        public static string Integral(int[] koefisien)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < koefisien.Length; i++)
+            {
+                int pangkat = koefisien.Length - i;
+                double hasil = (double)koefisien[i] / pangkat;
+
+                if (hasil == 0) continue;
+
+                if (sb.Length > 0)
+                    sb.Append(hasil > 0 ? " + " : " - ");
+                else if (hasil < 0)
+                    sb.Append("-");
+
+                sb.Append(Math.Abs(hasil));
+                if (pangkat > 1)
+                    sb.Append("x" + pangkat);
+            }
+
+            sb.Append(" + C");
+            return sb.ToString();
+        }
+    }
+}
---
+++ 10_Integrated_Project_Implementation/Modul10_231110466/PemanggilanLibrary/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System;
+using MatematikaLibraries;
+
+namespace Modul10_12345678
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FPB(60, 45): " + Matematika.FPB(60, 45));
+            Console.WriteLine("KPK(12, 8): " + Matematika.KPK(12, 8));
+
+            int[] fungsi1 = { 1, 4, -12, 9 };
+            Console.WriteLine("Turunan dari x^3 + 4x^2 - 12x + 9:");
+            Console.WriteLine(Matematika.Turunan(fungsi1));
+
+            int[] fungsi2 = { 4, 6, -12, 9 };
+            Console.WriteLine("Integral dari 4x^3 + 6x^2 - 12x + 9:");
+            Console.WriteLine(Matematika.Integral(fungsi2));
+        }
+    }
+}
